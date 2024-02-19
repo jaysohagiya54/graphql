d39e632f04a5862043e891d642d907b956004da8
jaysohagiya54 <jaysuhagiya80@gmail.com>
2024-02-19 16:38:49 +0530

    feat: Add Authentication/Authorization in user-model

--- threads-clone-backend/src/index.ts
+++ threads-clone-backend/src/index.ts
@@ -1,49 +1,32 @@
 import express from 'express'
-import { ApolloServer } from '@apollo/server';
 import { expressMiddleware } from '@apollo/server/express4';
-import { prismaClient } from './lib/db';
+import createApolloGraphqlServer from './graphql/index'
+import UserService from "./services/user";
 
 async function init(){
   const app = express()
   const port = 4000;
   
-  const gqlServer  = new ApolloServer({
-    typeDefs:`
-     type Query {
-        hello:String
-        say(name:String):String
-     }
-     type Mutation {
-       createUser(firstName:String!,lastName:String!,email:String!,password:String!) : Boolean
-     }
-    `,//schema
-    resolvers:{
-     Query :{
-     hello : () => `Hey There, I am A Graphql Server`,
-     say: (_,{name}:{name:string}) => `Hey , ${name} How are you?`
-    },
-    Mutation:{
-      createUser: async(_,{firstName,lastName,email,password}:{firstName:string;lastName:string,email:string,password:string})  => {
-        await prismaClient.user.create({
-          data: {
-            email,
-            firstName,
-            lastName,
-            password,
-            salt:'random_salt',
-          },
-        });
-        return true;  
-      }
-       }
-    } //actual functions to work
-  })
-  await gqlServer.start()
-  
+  const gqlServer = await createApolloGraphqlServer();
   app.get("/",(req: any,res: any) => {
     res.json({message:"Server is up and running"})
   });
-  app.use('/graphql', express.json(), expressMiddleware(gqlServer));
+  app.use(
+    "/graphql",express.json(),
+    expressMiddleware(await createApolloGraphqlServer(), {
+      context: async ({ req }) => {
+        // @ts-ignore
+        const token = req.headers["token"];
+
+        try {
+          const user = UserService.decodeJWTToken(token as string);
+          return { user };
+        } catch (error) {
+          return {};
+        }
+      },
+    })
+  );
   
   app.listen(port,()=> console.log(`Server running on port:${port}`))
 }
